3758de4e44fd27e4af3e2c15e65015b0c9eeb18c
Pedro Cardoso <phlc.cardoso@gmail.com>
2021-10-20 18:05:50 -0300

    Desafio 01 aula 03

---
+++ src/aula03/mesa_de_trabalho/Desafio1.java
@@ -0,0 +1,46 @@
+package aula03.mesa_de_trabalho;
+
+import java.util.Scanner;
+
+public class Desafio1 {
+  public static void main(String[] args) {
+    // Inicialização do Scanner
+    Scanner sc = new Scanner(System.in);
+
+    // Input do valor de "n" pelo usuário
+    System.out.printf("Digite a quantidade de números primos a ser mostrada: ");
+    int n = sc.nextInt();
+
+    // Variáveis auxiliares
+    int cont = 0;
+    int aux = 1;
+
+    // Seleçãpo e impressão dos números primos
+    while(cont < n){
+      if(isPrime(aux)){
+        System.out.println(aux);
+        cont++;
+      }
+      aux++;
+    }
+  }
+
+  public static boolean isPrime(int n){
+    // Variável de controle
+    int qtdDivisores = 0;
+
+    // Lógica para analisar quantos divisores tem o número
+    for(int i = 1; i <=n; i++){
+      if(n % i == 0){
+        qtdDivisores++;
+      }
+    }
+
+    // Análise da quantidade de divisores
+    if(qtdDivisores > 2 || qtdDivisores == 1){
+      return false;
+    }else{
+      return true;
+    }
+  }
+}
